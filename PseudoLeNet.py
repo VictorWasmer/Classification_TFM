c5cd4f47090fd606f75b3d8387f3980eaec5354c
VictorWasmer <victor.wasmer@gmail.com>
2021-11-09 12:16:13 +0100

    LeNet for RGB case and clean classification.py

--- PseudoLeNet.py
+++ PseudoLeNet.py
@@ -8,7 +8,7 @@ class PseudoLeNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.pad = nn.ConstantPad2d(padding=2, value=0)
-        self.conv1 = ConvBlock(1, 6, 5)
+        self.conv1 = ConvBlock(3, 6, 5)
         self.conv2 = ConvBlock(6, 16, 5)
         self.mlp = nn.Sequential(
             nn.Linear(16 * 5 * 5, 120),
--- classification.py
+++ classification.py
@@ -1,3 +1,4 @@
+import torch
 from torch import optim
 from Custom_Dataset import CustomImageDataset
 from torch.utils.data import DataLoader
@@ -24,28 +25,29 @@ my_dataset = CustomImageDataset(annotations_file=paths.annotation_path,
 # Split train/val sets
 train_set, val_set = split_dataset(my_dataset, 0.8)
 
-# Dataloader creation
-train_loader = DataLoader(train_set, batch_size=hparams['batch_size'], shuffle=True)
-val_loader = DataLoader(val_set, batch_size=hparams['batch_size'], shuffle=True)
+#Create a short subset to make faster tests
+short_trainset = torch.utils.data.Subset(train_set, [0,1,2,3,4,5,6,7,8,9]) 
+short_valset = torch.utils.data.Subset(val_set, [0,1,2,3,4,5,6,7,8,9])
 
-# dataloaders_dict = {'train': train_loader,'val': val_loader}
+# Dataloader creation
+train_loader = DataLoader(short_trainset, batch_size=hparams['batch_size'], shuffle=True)
+val_loader = DataLoader(short_valset, batch_size=hparams['batch_size'], shuffle=True)
 
 # Instantiate the model and modify the last layer to our specific case
-#model = models.mobilenet_v3_small(pretrained=True)
-#model.classifier[3] = nn.Linear(in_features=1024, out_features=hparams['num_classes'], bias=True)
-model = PseudoLeNet()
+model = models.mobilenet_v3_small(pretrained=True)
+model.classifier[3] = nn.Linear(in_features=1024, out_features=hparams['num_classes'], bias=True)
 # Send the model to GPU
-# model = model.to(hparams['device'])
-
+model.to(hparams['device'])
 
 # Set the transfer learning as feature extractor
 # feature_extract = True
 # Set all req_grad at False
-#for param in model.parameters():
-#    param.requires_grad = False
-# set_parameter_requires_grad(model, feature_extract)
+for param in model.parameters():
+    param.requires_grad = False
+
 # We only want to train the classifier part
-#model.classifier.requires_grad_()
+model.classifier.requires_grad_()
+
 params_to_update = []
 print("Params to learn:")
 
@@ -54,19 +56,12 @@ for name, param in model.named_parameters():
         params_to_update.append(param)
         print("\t", name)
 
-
-
-# track_params = { key_track: hparams[key_track] for key_track in params_to_track }
-
-# Train model
-# model_ft, hist = train_model(model, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs,
-#                             params_to_track=track_params)
-model.to(hparams['device'])
 # Setup the loss fxn
 criterion = F.nll_loss
-# Setup the optimizer
+# Set the optimizer
 #optimizer = optim.SGD(params_to_update, lr=hparams['learning_rate'], momentum=hparams['momentum'])
 optimizer = optim.Adam(params_to_update, lr=hparams['learning_rate'])
+
 for epoch in range(1, hparams['num_epochs'] + 1):
     tr_loss, tr_acc = train_epoch(train_loader, model, optimizer, criterion, hparams)
     wandb.log({"Epoch Train Loss": tr_loss,
