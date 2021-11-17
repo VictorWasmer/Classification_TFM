dda69e01be13fd005663934b204fb5d3a421472b
VictorWasmer <victor.wasmer@gmail.com>
2021-11-17 12:40:27 +0100

    short dataset added

--- aux_functions.py
+++ aux_functions.py
@@ -1,13 +1,76 @@
 import torch
-import time
 import copy
 from AverageMeter import AverageMeter
-import definitions
-import wandb
 
+
+def train_model(model, optimizer, loss_fn, train_loader, val_loader, hparams, wandb):
+
+    train_accuracies, train_losses, val_accuracies, val_losses = [], [], [], []
+    val_loss = AverageMeter()
+    val_accuracy = AverageMeter()
+    train_loss = AverageMeter()
+    train_accuracy = AverageMeter()
+
+    for epoch in range(hparams['num_epochs']):
+        # train
+        model.train()
+        train_loss.reset()
+        train_accuracy.reset()
+        for data, target in train_loader:
+            data, target = data.float().to(
+                hparams['device']), target.float().to(hparams['device'])
+            target = target.unsqueeze(-1)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_fn(output, target)
+            loss.backward()
+            optimizer.step()
+            train_loss.update(loss.item(), n=len(target))
+            pred = output.round()  # get the prediction
+            acc = pred.eq(target.view_as(pred)).sum().item()/len(target)
+            train_accuracy.update(acc, n=len(target))
+
+        train_losses.append(train_loss.avg)
+        train_accuracies.append(train_accuracy.avg)
+        wandb.log({"Epoch Train Loss": train_loss.avg,
+                  "Epoch Train Accuracy": train_accuracy.avg})
+
+        # validation
+        model.eval()
+        val_loss.reset()
+        val_accuracy.reset()
+        with torch.no_grad():
+            for data, target in val_loader:
+                data, target = data.float().to(
+                    hparams['device']), target.float().to(hparams['device'])
+                target = target.unsqueeze(-1)
+                output = model(data)
+                loss = loss_fn(output, target)
+                val_loss.update(loss.item(), n=len(target))
+                pred = output.round()  # get the prediction
+                acc = pred.eq(target.view_as(pred)).sum().item()/len(target)
+                val_accuracy.update(acc, n=len(target))
+
+        val_losses.append(val_loss.avg)
+        val_accuracies.append(val_accuracy.avg)
+        wandb.log({"Epoch Validation Loss": val_loss.avg,
+                  "Epoch Validation Accuracy": val_accuracy.avg})
+
+    return train_accuracies, train_losses, val_accuracies, val_losses
+
+
+def split_dataset(dataset, train_portion):
+    train_size = int(train_portion * len(dataset))
+    val_size = len(dataset) - train_size
+    train_set, val_set = torch.utils.data.random_split(
+        dataset, [train_size, val_size])
+    return train_set, val_set
+
+#!From here to the end is deprecated code
 
 def correct_predictions(predicted_batch, label_batch):
-    pred = predicted_batch.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    # get the index of the max log-probability
+    pred = predicted_batch.argmax(dim=1, keepdim=True)
     acum = pred.eq(label_batch.view_as(pred)).sum().item()
     return acum
 
@@ -23,7 +86,8 @@ def train_epoch(train_loader, model, optimizer, criterion, hparams):
     acc = 0
 
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.float().to(hparams['device']), target.float().to(hparams['device'])
+        data, target = data.float().to(
+            hparams['device']), target.float().to(hparams['device'])
         optimizer.zero_grad()
         output = model(data)
         target = target.unsqueeze(-1)
@@ -49,10 +113,12 @@ def val_epoch(val_loader, model, criterion, hparams):
     acc = 0
     with torch.no_grad():
         for data, target in val_loader:
-            data, target = data.float().to(hparams['device']), target.float().to(hparams['device'])
+            data, target = data.float().to(
+                hparams['device']), target.float().to(hparams['device'])
             output = model(data)
             target = target.unsqueeze(-1)
-            val_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            # sum up batch loss
+            val_loss += criterion(output, target, reduction='sum').item()
             # compute number of correct predictions in the batch
             acc += correct_predictions(output, target)
     # Average acc across all correct predictions batches now
@@ -62,70 +128,3 @@ def val_epoch(val_loader, model, criterion, hparams):
         val_loss, acc, len(val_loader.dataset), val_acc,
     ))
     return val_loss, val_acc
-
-
-def train_model(model, optimizer, loss_fn, train_loader, val_loader, hparams, wandb):
-
-    train_accuracies, train_losses, val_accuracies, val_losses = [], [], [], []
-    val_loss = AverageMeter()
-    val_accuracy = AverageMeter()
-    train_loss = AverageMeter()
-    train_accuracy = AverageMeter()
-
-    for epoch in range(hparams['num_epochs']):
-        # train
-        model.train()
-        train_loss.reset()
-        train_accuracy.reset()
-        for data, target in train_loader:
-            data, target = data.float().to(hparams['device']), target.float().to(hparams['device'])
-            target = target.unsqueeze(-1)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = loss_fn(output, target)
-            loss.backward()
-            optimizer.step()
-            train_loss.update(loss.item(), n=len(target))
-            pred = output.round()  # get the prediction
-            acc = pred.eq(target.view_as(pred)).sum().item()/len(target)
-            train_accuracy.update(acc, n=len(target))
-
-        train_losses.append(train_loss.avg)
-        train_accuracies.append(train_accuracy.avg)
-        wandb.log({"Epoch Train Loss": train_loss.avg, "Epoch Train Accuracy": train_accuracy.avg})
-
-
-        # validation
-        model.eval()
-        val_loss.reset()
-        val_accuracy.reset()
-        with torch.no_grad():
-            for data, target in val_loader:
-                data, target = data.float().to(hparams['device']), target.float().to(hparams['device'])
-                target = target.unsqueeze(-1)
-                output = model(data)
-                loss = loss_fn(output, target)
-                val_loss.update(loss.item(), n=len(target))
-                pred = output.round()  # get the prediction
-                acc = pred.eq(target.view_as(pred)).sum().item()/len(target)
-                val_accuracy.update(acc, n=len(target))
-
-        val_losses.append(val_loss.avg)
-        val_accuracies.append(val_accuracy.avg)
-        wandb.log({"Epoch Validation Loss": val_loss.avg, "Epoch Validation Accuracy": val_accuracy.avg})
-
-    return train_accuracies, train_losses, val_accuracies, val_losses
-
-    
-def set_parameter_requires_grad(model, feature_extracting):
-    if feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
-
-
-def split_dataset(dataset, train_portion):
-    train_size = int(train_portion * len(dataset))
-    val_size = len(dataset) - train_size
-    train_set, val_set = torch.utils.data.random_split(dataset, [train_size, val_size])
-    return train_set, val_set
-
--- classification.py
+++ classification.py
@@ -6,18 +6,18 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import torchvision.models as models
 import torch.nn as nn
-import torch.nn.functional as F
 import time
 from definitions import hparams, params_to_track
 import paths
-from aux_functions import split_dataset, train_epoch, train_model, val_epoch
+from aux_functions import split_dataset, train_model
 import wandb
 
 track_params = {key_track: hparams[key_track] for key_track in params_to_track}
-wandb.init(project="Classification_TFM", entity="viiiictorr", config=track_params)
+wandb.init(project="Classification_TFM",
+           entity="viiiictorr", config=track_params)
 
 transformations = transforms.Compose([transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                            std=[0.229, 0.224, 0.225])])
+                                                           std=[0.229, 0.224, 0.225])])
 # Instantiation of the dataset
 my_dataset = CustomImageDataset(annotations_file=paths.annotation_path,
                                 img_dir=paths.img_path,
@@ -25,19 +25,22 @@ my_dataset = CustomImageDataset(annotations_file=paths.annotation_path,
 # Split train/val sets
 train_set, val_set = split_dataset(my_dataset, 0.8)
 
-#Create a short subset to make faster tests
-short_trainset = torch.utils.data.Subset(train_set, [0,1,2,3,4,5,6,7,8,9]) 
-short_valset = torch.utils.data.Subset(val_set, [10,11,12,13,14,15,16,17,18,19])
+# Create a short subset to make faster tests
+#short_trainset = torch.utils.data.Subset(train_set, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+#short_valset = torch.utils.data.Subset(val_set, [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
 
 # Dataloader creation
-train_loader = DataLoader(short_trainset, batch_size=hparams['batch_size'], shuffle=True)
-val_loader = DataLoader(short_valset, batch_size=hparams['batch_size'], shuffle=True)
+train_loader = DataLoader(
+    train_set, batch_size=hparams['batch_size'], shuffle=True)
+val_loader = DataLoader(
+    val_set, batch_size=hparams['batch_size'], shuffle=True)
 
 # Instantiate the model and modify the last layer to our specific case
 model = models.mobilenet_v3_small(pretrained=True)
 model.classifier[3] = nn.Sequential(
-                        nn.Linear(in_features=1024, out_features=hparams['num_classes'], bias=True),
-                        nn.Sigmoid())
+    nn.Linear(in_features=1024,
+              out_features=hparams['num_classes'], bias=True),
+    nn.Sigmoid())
 
 # Send the model to GPU
 model.to(hparams['device'])
@@ -57,24 +60,17 @@ for name, param in model.named_parameters():
         params_to_update.append(param)
         print("\t", name)
 
-# Setup the loss fxn
+# Setup the loss function
 criterion = nn.BCELoss()
 # Set the optimizer
 optimizer = optim.Adam(params_to_update, lr=hparams['learning_rate'])
 
+#Add the loss function and the optimizer to de wandb config file
 wandb.config.update({"Loss function": criterion, "Optimizer": optimizer})
 
-train_accuracies, train_losses, val_accuracies, val_losses = train_model(model, optimizer, criterion, train_loader, val_loader, hparams,wandb)
-
-""" for epoch in range(1, hparams['num_epochs'] + 1):
-    tr_loss, tr_acc = train_epoch(train_loader, model, optimizer, criterion, hparams)
-    wandb.log({"Epoch Train Loss": tr_loss,
-               "Epoch Train Accuracy": tr_acc})
-    val_loss, val_acc = val_epoch(val_loader, model, criterion, hparams)
-    wandb.log({"Epoch Val Loss": tr_loss,
-               "Epoch Val Accuracy": tr_acc}) """
+train_accuracies, train_losses, val_accuracies, val_losses = train_model(
+    model, optimizer, criterion, train_loader, val_loader, hparams, wandb)
 
 model_date = time.strftime("%Y%m%d-%H%M%S")
 filename = "model_%s.pt" % model_date
 torch.save(model.state_dict(), os.path.join("models", filename))
-
