5101c7dbe9501ef147b5ebbe16924bf8005b7aaa
VictorWasmer <victor.wasmer@gmail.com>
2022-02-04 08:27:44 +0100

    .

--- Main.py
+++ Main.py
@@ -19,12 +19,12 @@ parser.add_argument('--epochs', default=hparams['num_epochs'], type=int, metavar
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch_size', default=hparams['batch_size'], type=int,
+parser.add_argument('-b', '--batch-size', default=hparams['batch_size'], type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--learning_rate', default=hparams['learning_rate'], type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--momentum', default=hparams['momentum'], type=float, metavar='M',
                     help='momentum')
