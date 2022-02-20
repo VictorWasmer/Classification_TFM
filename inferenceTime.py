9d24e0fd89675e7701e30599b1887be62e2430dc
VictorWasmer <victor.wasmer@gmail.com>
2022-02-20 17:54:52 +0100

    .

--- inferenceTime.py
+++ inferenceTime.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 from torchvision import models
+import numpy as np
+
+np.random.seed(0)
 
 model = models.mobilenet_v3_large(pretrained=True)
 model.classifier[3] = nn.Sequential(
@@ -8,21 +11,25 @@ model.classifier[3] = nn.Sequential(
     nn.Sigmoid())    
 
 device = torch.device('cuda')
-
 model.load_state_dict(torch.load('models\final_model_20220217-010634.pt', map_location=device))
 model.to(device)
-
-dummy_input = torch.randn(64, 3,224,224, dtype=torch.float).to(device)
-repetitions=100
-total_time = 0
+dummy_input = torch.randn(1, 3,224,224,dtype=torch.float).to(device)
+starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+repetitions = 300
+timings=np.zeros((repetitions,1))
+#GPU-WARM-UP
+for _ in range(10):
+   _ = model(dummy_input)
+# MEASURE PERFORMANCE
 with torch.no_grad():
   for rep in range(repetitions):
-     starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
      starter.record()
      _ = model(dummy_input)
      ender.record()
+     # WAIT FOR GPU SYNC
      torch.cuda.synchronize()
-     curr_time = starter.elapsed_time(ender)/1000
-     total_time += curr_time
-Throughput = (repetitions*64)/total_time
-print(f'Final Throughput: {Throughput}', flush=True)
+     curr_time = starter.elapsed_time(ender)
+     timings[rep] = curr_time
+mean_syn = np.sum(timings) / repetitions
+std_syn = np.std(timings)
+print(mean_syn)
